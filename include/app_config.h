3f7703324a74bd779034d96a6612bbf76086e2d3
Thomas <thomas@socraft.ch>
2026-05-04 15:01:49 +0200

    chore(firmware): bump version to 0.2.1

--- include/app_config.h
+++ include/app_config.h
@@ -13,7 +13,7 @@
 #define APP_VERSION_MAJOR 0
 #define APP_VERSION_MINOR 1
 #define APP_VERSION_PATCH 0
-#define APP_VERSION_STR "0.2.0"
+#define APP_VERSION_STR "0.2.1"
 
 // ---------------------------------------------------------------------------
 // Build mode flags
