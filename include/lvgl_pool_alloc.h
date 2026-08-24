6d58367d347dbf1d0976e69994470bb36e997b35
Thomas <thomas@tmbk.ch>
2026-08-24 11:23:13 +0200

    fix(firmware): the LVGL pool allocator never hands tlsf a null pointer

---
+++ include/lvgl_pool_alloc.h
@@ -0,0 +1,17 @@
+
+#ifndef CANSHIFT_LVGL_POOL_ALLOC_H
+#define CANSHIFT_LVGL_POOL_ALLOC_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void *canshift_lvgl_pool_alloc(size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
